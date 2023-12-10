41c0ab384c2f3bd9b485fffcafdc12b09a970495
Hasmuddin <hasmuddin97175@gmail.com>
2023-12-10 18:14:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./Components/Navbar"
+import FAQ from "./Components/FAQ"
+import Pricing from "./Components/Pricing"
+import Footer from "./Components/Footer"
+
+function App() {
+
+  return (
+    <>
+<Navbar/>     
+<Pricing/>
+<FAQ/>     
+<Footer/>
+    </>
+  )
+}
+
+export default App
